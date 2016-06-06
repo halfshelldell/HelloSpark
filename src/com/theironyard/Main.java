b133e029235b6c3ec719a411854aedd6111a4ab4
halfshelldell <simmonslydell@gmai.com>
2016-06-06 11:44:31 -0400

    import added loginpage

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,8 +1,42 @@
 package com.theironyard;
 
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.HashMap;
+
 public class Main {
 
+    static User user;
+
     public static void main(String[] args) {
-	// write your code here
+        /** GET: Routes returns html
+         ** POST: Routes modify the server(data)
+         */
+        Spark.init();
+        Spark.get(
+                 "/",
+                 (request, response) -> {
+                     HashMap m = new HashMap();
+                     if (user == null) {
+                         return new ModelAndView(m, "loginpage.html");
+                     }
+                     else {
+                         m.put("name", user.name);
+                         return new ModelAndView(m, "home.html");
+                     }
+                 },
+                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                 "/login",
+                 (request, response) -> {
+                    String username = request.queryParams("username");
+                     user = new User(username);
+                     response.redirect("/");
+                     return "";
+                 }
+        );
     }
 }
